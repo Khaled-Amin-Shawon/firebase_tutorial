82fe63f502e6c572b46fa56278581647cf516e94
Khaled-Amin-Shawon <mdkhaledaminshawon@gmail.com>
2025-03-13 08:39:38 +0600

    Add reset password functionality to Firebase authentication service

--- lib/firebase_service/firebase_authentication.dart
+++ lib/firebase_service/firebase_authentication.dart
@@ -78,4 +78,15 @@ class FirebaseAuthentication {
         return 'Authentication error: ${e.message}';
     }
   }
+  // reset password by firebase authentication
+  Future<Map<String, dynamic>> resetPassword(String email) async {
+    try {
+      await _auth.sendPasswordResetEmail(email: email);
+      return {'success': true, 'message': 'Password reset email sent. check your email'};
+    } on FirebaseAuthException catch (e) {
+      return {'success': false, 'message': _handleAuthException(e)};
+    } catch (e) {
+      return {'success': false, 'message': 'An unexpected error occurred: $e'};
+    }
+  }
 }
